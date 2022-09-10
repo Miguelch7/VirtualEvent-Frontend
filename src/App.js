ec549575a29406dcebd513cb9e49a160374de393
Miguel <chanampamiguel@gmail.com>
2022-09-10 11:19:20 -0300

    Create Main Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import Hero from './components/Hero';
+import Main from './components/Main';
 
 function App() {
   return (
     <>
       <Hero />
+
+      <section className="ProximaNovaFont flex flex-wrap justify-between items-center text-sm w-4/5 m-auto py-10 lg:flex-nowrap">
+        <Main />
+      </section>
     </>
   );
 }
