c6e9e0b3911d243eca7e1dd1d1fb236c85f1c92f
Miguel <chanampamiguel@gmail.com>
2022-09-17 16:01:16 -0300

    Phone validation

--- src/helpers/validateForm.js
+++ src/helpers/validateForm.js
@@ -1,11 +1,12 @@
 const regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+const regexPhone = /^[+]?[0-9]{10,13}$/;
 
 const validationsMessages = {
   name: 'El Nombre es obligatorio',
   surname: 'El Apellido es obligatorio',
-  email: 'El Email debe ser válido: "example@example.com',
+  email: 'El Email debe ser válido: "example@example.com"',
   country: 'El País es obligatorio',
-  phone: 'El Número de Telefono debe contener al menos 10 números',
+  phone: 'El Número de Telefono debe tener entre 10 y 13 números',
   job: 'El campo Puesto de Trabajo es obligatorio',
 }
 
@@ -14,7 +15,7 @@ const validations = {
   surname: value => !value?.trim(),
   email: value => !regexEmail.test(value),
   country: value => !value?.trim(),
-  phone: value => !value?.trim(),
+  phone: value => !regexPhone.test(value),
   job: value => !value?.trim()
 }
 
